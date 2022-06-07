c38902199c8c5a204a06a2f024ff795cc63d52ff
M Monty <79332342+MM0nty@users.noreply.github.com>
2022-06-07 18:40:18 -0400

    Add comments

--- LeftRotation.js
+++ LeftRotation.js
@@ -2,8 +2,8 @@ function rotLeft(a, d) {
     // Write your code here
     let array = [];
 for (let i = 0; i < d; i++) {
-    array = a.shift();
-    a.push(array);
+    array = a.shift(); //Remove element at index 0
+    a.push(array); //Add removed element at end of array
 }
 console.log(a);
 return a;
