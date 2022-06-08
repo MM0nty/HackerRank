8b8049e8593d3ee6994588092eefdf2a3d729323
M Monty <79332342+MM0nty@users.noreply.github.com>
2022-06-08 19:57:31 -0400

    Add comments

--- SalesMatch.js
+++ SalesMatch.js
@@ -1,12 +1,17 @@
 function sockMerchant(n, ar) {
-    // Write your code here
-    let pairs = 0;
-    ar.sort();
-    for (let i = 0; i < n; i++) {
-        if (ar[i] === ar[i + 1]) {
-            pairs++;
-            i++;
-        }
+  // Write your code here
+  let pairs = 0;
+  //sort array
+  ar.sort();
+  for (let i = 0; i < n; i++) {
+    //if pair is found
+    if (ar[i] === ar[i + 1]) {
+      //add to pairs
+      pairs++;
+      //skip next index (since that was what was paired)
+      i++;
     }
-    return pairs;
+  }
+  console.log(pairs);
+  return pairs;
 }
