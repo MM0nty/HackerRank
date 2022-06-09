792200ae7ab7e2ce8813b61a052db8159a09aa06
M Monty <79332342+MM0nty@users.noreply.github.com>
2022-06-08 20:00:33 -0400

    Fix indentation

--- LeftRotation.js
+++ LeftRotation.js
@@ -1,13 +1,12 @@
 function rotLeft(a, d) {
-    // Write your code here
-    let array = [];
-for (let i = 0; i < d; i++) {
+  let array = [];
+  for (let i = 0; i < d; i++) {
     //remove element at index 0
     array = a.shift();
     //add removed element at end of array
     a.push(array);
-}
-console.log(a);
-//return array
-return a;
+  }
+  console.log(a);
+  //return array
+  return a;
 }
