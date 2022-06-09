0b06d60ad06e2f6076bc382cb92eb31250f1e564
M Monty <79332342+MM0nty@users.noreply.github.com>
2022-06-09 16:36:23 -0400

    Add comments

--- TwoStrings.js
+++ TwoStrings.js
@@ -1,8 +1,10 @@
 function twoStrings(s1, s2) {
     let letter = {};
+    //values of s1 and s2
     console.log(s1);
     console.log(s2);
     for (let i in s1) {
+        //s2 has s1[i] anywhere
         if (s2.includes(s1[i])) {
             return "YES";
         }
