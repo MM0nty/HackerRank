1b9f3342a9bf2ae04010294062bc2b706a110778
M Monty <79332342+MM0nty@users.noreply.github.com>
2022-06-09 16:08:53 -0400

    Add comments

--- RansomNote.js
+++ RansomNote.js
@@ -1,25 +1,34 @@
 function checkMagazine(magazine, note) {
     let words = {};
+    //loop through note
     for (let i of note) {
+        //if words[i] is not found, initialize as 1
         if (!words[i]) {
-            words[i] += 1;
+            words[i] = 1;
         }
+        //if words[i] is found, add 1
         else {
             words[i] = words[i] + 1;
         }
     }
+    //loop through magazine
     for (let i of magazine) {
+        //if words[i] is not found, initialize as -1
         if (!words[i]) {
-            words[i] -= 1;
+            words[i] = - 1;
         }
+        //if words[i] is found, subtract 1
         else {
             words[i] = words[i] - 1;
         }
     }
+    //loop through words
     for (let i in words) {
+        //note words not found in words object have value of < 0
         if (words[i] > 0) {
             return console.log("No");
         }
     }
+    //note words are all found in magazine
     return console.log("Yes");
 }
