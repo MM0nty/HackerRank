594c43981e1effb2fe8cbe0ed6f80092ff88a3eb
M Monty <79332342+MM0nty@users.noreply.github.com>
2022-06-10 10:32:23 -0400

    Add comments

--- Alternating.js
+++ Alternating.js
@@ -1,6 +1,7 @@
 function alternatingCharacters(s) {
     let count = 0;
     for (let i = 0; i < s.length; i++) {
+        //if the same character is next, count++
         if (s[i] === s[i + 1]) {
             count++;
         }
